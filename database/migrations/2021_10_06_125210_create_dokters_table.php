ea873252f1adbad65cd7436f2c5b62de7c07dc49
rozakira <rozakira.220180133@mhs.unimal.ac.id>
2024-06-22 17:15:18 +0700

    Add files via upload

--- database/migrations/2021_10_06_125210_create_dokters_table.php
+++ database/migrations/2021_10_06_125210_create_dokters_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateDoktersTable extends Migration
 {
   /**
    * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
    */
   public function up()
   {
-    Schema::create('users', function (Blueprint $table) {
+    Schema::create('dokters', function (Blueprint $table) {
       $table->bigIncrements('id');
-      $table->string('name');
-      $table->string('email')->unique();
-      $table->timestamp('email_verified_at')->nullable();
-      $table->string('password');
-      $table->enum('role', ['admin', 'pasien', 'dokter'])->default('pasien');
-      $table->rememberToken();
+      $table->string('nama_dokter');
+      $table->string('spesialisasi_dokter');
+      $table->string('alamat_dokter');
       $table->timestamps();
     });
   }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
    */
   public function down()
   {
-    Schema::dropIfExists('users');
+    Schema::dropIfExists('dokters');
   }
 }
--- database/migrations/2021_10_06_125401_create_obats_table.php
+++ database/migrations/2021_10_06_125401_create_obats_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateObatsTable extends Migration
 {
   /**
    * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
    */
   public function up()
   {
-    Schema::create('users', function (Blueprint $table) {
+    Schema::create('obats', function (Blueprint $table) {
       $table->bigIncrements('id');
-      $table->string('name');
-      $table->string('email')->unique();
-      $table->timestamp('email_verified_at')->nullable();
-      $table->string('password');
-      $table->enum('role', ['admin', 'pasien', 'dokter'])->default('pasien');
-      $table->rememberToken();
+      $table->string('nama_obat');
+      $table->integer('jumlah_obat');
+      $table->integer('harga_obat');
+      // $table->unsignedBigInteger('pasien_id')->nullable();
+      // $table->foreign('pasien_id')->references('id')->on('pasiens')->onDelete('cascade');
       $table->timestamps();
     });
   }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
    */
   public function down()
   {
-    Schema::dropIfExists('users');
+    Schema::dropIfExists('obats');
   }
 }
