75bc4c914a4ab209ce78f135d54c230451e4465c
rozakira <rozakira.220180133@mhs.unimal.ac.id>
2024-06-23 19:28:50 +0700

    Add files via upload

--- vendor/laravel/framework/src/Illuminate/Auth/Notifications/VerifyEmail.php
+++ vendor/laravel/framework/src/Illuminate/Auth/Notifications/VerifyEmail.php
@@ -4,19 +4,15 @@ namespace Illuminate\Auth\Notifications;
 
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\URL;
 
-class ResetPassword extends Notification
+class VerifyEmail extends Notification
 {
     /**
-     * The password reset token.
-     *
-     * @var string
-     */
-    public $token;
-
-    /**
-     * The callback that should be used to create the reset password URL.
+     * The callback that should be used to create the verify email URL.
      *
      * @var \Closure|null
      */
@@ -28,17 +24,6 @@ class ResetPassword extends Notification
      * @var \Closure|null
      */
     public static $toMailCallback;
-
-    /**
-     * Create a notification instance.
-     *
-     * @param  string  $token
-     * @return void
-     */
-    public function __construct($token)
-    {
-        $this->token = $token;
-    }
 
     /**
      * Get the notification's channels.
@@ -59,15 +44,17 @@ class ResetPassword extends Notification
      */
     public function toMail($notifiable)
     {
+        $verificationUrl = $this->verificationUrl($notifiable);
+
         if (static::$toMailCallback) {
-            return call_user_func(static::$toMailCallback, $notifiable, $this->token);
+            return call_user_func(static::$toMailCallback, $notifiable, $verificationUrl);
         }
 
-        return $this->buildMailMessage($this->resetUrl($notifiable));
+        return $this->buildMailMessage($verificationUrl);
     }
 
     /**
-     * Get the reset password notification mail message for the given URL.
+     * Get the verify email notification mail message for the given URL.
      *
      * @param  string  $url
      * @return \Illuminate\Notifications\Messages\MailMessage
@@ -75,33 +62,36 @@ class ResetPassword extends Notification
     protected function buildMailMessage($url)
     {
         return (new MailMessage)
-            ->subject(Lang::get('Reset Password Notification'))
-            ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::get('Reset Password'), $url)
-            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-            ->line(Lang::get('If you did not request a password reset, no further action is required.'));
+            ->subject(Lang::get('Verify Email Address'))
+            ->line(Lang::get('Please click the button below to verify your email address.'))
+            ->action(Lang::get('Verify Email Address'), $url)
+            ->line(Lang::get('If you did not create an account, no further action is required.'));
     }
 
     /**
-     * Get the reset URL for the given notifiable.
+     * Get the verification URL for the given notifiable.
      *
      * @param  mixed  $notifiable
      * @return string
      */
-    protected function resetUrl($notifiable)
+    protected function verificationUrl($notifiable)
     {
         if (static::$createUrlCallback) {
-            return call_user_func(static::$createUrlCallback, $notifiable, $this->token);
+            return call_user_func(static::$createUrlCallback, $notifiable);
         }
 
-        return url(route('password.reset', [
-            'token' => $this->token,
-            'email' => $notifiable->getEmailForPasswordReset(),
-        ], false));
+        return URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+            ]
+        );
     }
 
     /**
-     * Set a callback that should be used when creating the reset password button URL.
+     * Set a callback that should be used when creating the email verification URL.
      *
      * @param  \Closure  $callback
      * @return void
