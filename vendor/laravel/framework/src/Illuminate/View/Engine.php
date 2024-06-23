61a5dfc75db502f3205720b1efd71bdf0a2957d6
rozakira <rozakira.220180133@mhs.unimal.ac.id>
2024-06-23 21:17:34 +0700

    Add files via upload

--- vendor/laravel/framework/src/Illuminate/View/Engine.php
+++ vendor/laravel/framework/src/Illuminate/View/Engine.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace Illuminate\Contracts\View;
+
+interface Engine
+{
+    /**
+     * Get the evaluated contents of the view.
+     *
+     * @param  string  $path
+     * @param  array  $data
+     * @return string
+     */
+    public function get($path, array $data = []);
+}
