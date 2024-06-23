0a50d41f6562f95998a3de4aa41625947973c67b
rozakira <rozakira.220180133@mhs.unimal.ac.id>
2024-06-23 20:04:21 +0700

    Add files via upload

--- vendor/laravel/framework/src/Illuminate/Console/Scheduling/CacheAware.php
+++ vendor/laravel/framework/src/Illuminate/Console/Scheduling/CacheAware.php
@@ -1,1 +1,14 @@
+<?php
 
+namespace Illuminate\Console\Scheduling;
+
+interface CacheAware
+{
+    /**
+     * Specify the cache store that should be used.
+     *
+     * @param  string  $store
+     * @return $this
+     */
+    public function useStore($store);
+}
