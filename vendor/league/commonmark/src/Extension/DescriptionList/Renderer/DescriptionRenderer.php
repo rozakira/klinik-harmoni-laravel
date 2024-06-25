b185a24384a15cba2ea16b1e648afb1e7ee048a0
rozakira <rozakira.220180133@mhs.unimal.ac.id>
2024-06-25 18:24:35 +0700

    Add files via upload

--- vendor/league/commonmark/src/Extension/DescriptionList/Renderer/DescriptionRenderer.php
+++ vendor/league/commonmark/src/Extension/DescriptionList/Renderer/DescriptionRenderer.php
@@ -13,27 +13,25 @@ declare(strict_types=1);
 
 namespace League\CommonMark\Extension\DescriptionList\Renderer;
 
-use League\CommonMark\Extension\DescriptionList\Node\DescriptionList;
+use League\CommonMark\Extension\DescriptionList\Node\Description;
 use League\CommonMark\Node\Node;
 use League\CommonMark\Renderer\ChildNodeRendererInterface;
 use League\CommonMark\Renderer\NodeRendererInterface;
 use League\CommonMark\Util\HtmlElement;
 
-final class DescriptionListRenderer implements NodeRendererInterface
+final class DescriptionRenderer implements NodeRendererInterface
 {
     /**
-     * @param DescriptionList $node
+     * @param Description $node
      *
      * {@inheritDoc}
      *
      * @psalm-suppress MoreSpecificImplementedParamType
      */
-    public function render(Node $node, ChildNodeRendererInterface $childRenderer): HtmlElement
+    public function render(Node $node, ChildNodeRendererInterface $childRenderer): \Stringable
     {
-        DescriptionList::assertInstanceOf($node);
+        Description::assertInstanceOf($node);
 
-        $separator = $childRenderer->getBlockSeparator();
-
-        return new HtmlElement('dl', [], $separator . $childRenderer->renderNodes($node->children()) . $separator);
+        return new HtmlElement('dd', [], $childRenderer->renderNodes($node->children()));
     }
 }
