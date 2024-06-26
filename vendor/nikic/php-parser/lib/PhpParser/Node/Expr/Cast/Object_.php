26f6454eeddac9438db4d103de6a0385e9a79bea
rozakira <rozakira.220180133@mhs.unimal.ac.id>
2024-06-26 20:10:56 +0700

    Add files via upload

--- vendor/nikic/php-parser/lib/PhpParser/Node/Expr/Cast/Object_.php
+++ vendor/nikic/php-parser/lib/PhpParser/Node/Expr/Cast/Object_.php
@@ -4,8 +4,8 @@ namespace PhpParser\Node\Expr\Cast;
 
 use PhpParser\Node\Expr\Cast;
 
-class Array_ extends Cast {
+class Object_ extends Cast {
     public function getType(): string {
-        return 'Expr_Cast_Array';
+        return 'Expr_Cast_Object';
     }
 }
--- vendor/nikic/php-parser/lib/PhpParser/Node/Expr/Cast/String_.php
+++ vendor/nikic/php-parser/lib/PhpParser/Node/Expr/Cast/String_.php
@@ -4,8 +4,8 @@ namespace PhpParser\Node\Expr\Cast;
 
 use PhpParser\Node\Expr\Cast;
 
-class Array_ extends Cast {
+class String_ extends Cast {
     public function getType(): string {
-        return 'Expr_Cast_Array';
+        return 'Expr_Cast_String';
     }
 }
