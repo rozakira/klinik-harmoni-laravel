29ca811b26a2208c7784276853af46befc0d9026
rozakira <rozakira.220180133@mhs.unimal.ac.id>
2024-06-26 18:08:31 +0700

    Add files via upload

--- vendor/nesbot/carbon/lazy/Carbon/MessageFormatter/MessageFormatterMapperWeakType.php
+++ vendor/nesbot/carbon/lazy/Carbon/MessageFormatter/MessageFormatterMapperWeakType.php
@@ -11,18 +11,26 @@
 
 namespace Carbon\MessageFormatter;
 
+use Symfony\Component\Translation\Formatter\ChoiceMessageFormatterInterface;
 use Symfony\Component\Translation\Formatter\MessageFormatterInterface;
 
 if (!class_exists(LazyMessageFormatter::class, false)) {
-    abstract class LazyMessageFormatter implements MessageFormatterInterface
+    abstract class LazyMessageFormatter implements MessageFormatterInterface, ChoiceMessageFormatterInterface
     {
-        public function format(string $message, string $locale, array $parameters = []): string
+        abstract protected function transformLocale(?string $locale): ?string;
+
+        public function format($message, $locale, array $parameters = [])
         {
             return $this->formatter->format(
                 $message,
                 $this->transformLocale($locale),
                 $parameters
             );
+        }
+
+        public function choiceFormat($message, $number, $locale, array $parameters = [])
+        {
+            return $this->formatter->choiceFormat($message, $number, $locale, $parameters);
         }
     }
 }
