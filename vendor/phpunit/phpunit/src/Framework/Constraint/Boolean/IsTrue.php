528d26fb04527015763e30338b06cb2703076ccc
rozakira <rozakira.220180133@mhs.unimal.ac.id>
2024-06-27 17:01:59 +0700

    Add files via upload

--- vendor/phpunit/phpunit/src/Framework/Constraint/Boolean/IsTrue.php
+++ vendor/phpunit/phpunit/src/Framework/Constraint/Boolean/IsTrue.php
@@ -12,14 +12,14 @@ namespace PHPUnit\Framework\Constraint;
 /**
  * @no-named-arguments Parameter names are not covered by the backward compatibility promise for PHPUnit
  */
-final class IsFalse extends Constraint
+final class IsTrue extends Constraint
 {
     /**
      * Returns a string representation of the constraint.
      */
     public function toString(): string
     {
-        return 'is false';
+        return 'is true';
     }
 
     /**
@@ -30,6 +30,6 @@ final class IsFalse extends Constraint
      */
     protected function matches($other): bool
     {
-        return $other === false;
+        return $other === true;
     }
 }
