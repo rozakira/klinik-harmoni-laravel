3d716a1ea1e32e44e0746b9404df4f96b0049bb2
rozakira <rozakira.220180133@mhs.unimal.ac.id>
2024-06-29 12:53:54 +0700

    Add files via upload

--- vendor/symfony/http-kernel/Attribute/AsController.php
+++ vendor/symfony/http-kernel/Attribute/AsController.php
@@ -11,13 +11,13 @@
 
 namespace Symfony\Component\HttpKernel\Attribute;
 
-trigger_deprecation('symfony/http-kernel', '5.3', 'The "%s" interface is deprecated.', ArgumentInterface::class);
-
 /**
- * Marker interface for controller argument attributes.
- *
- * @deprecated since Symfony 5.3
+ * Service tag to autoconfigure controllers.
  */
-interface ArgumentInterface
+#[\Attribute(\Attribute::TARGET_CLASS)]
+class AsController
 {
+    public function __construct()
+    {
+    }
 }
